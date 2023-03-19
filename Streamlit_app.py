6c4d0aa6fcea56d969c40c11d5af66823bf0bcd5
Anis Guechtouli <81176327+GuechtouliAnis@users.noreply.github.com>
2023-03-19 15:57:14 +0100

    update2

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,26 +1,38 @@
 import streamlit as st
+import pandas as pd
+import altair as alt
 
-# Define function for each page
-def page1():
-    st.title("Page 1")
-    st.write("Welcome to Page 1")
+# Load data
+data = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv")
 
-def page2():
-    st.title("Page 2")
-    st.write("Welcome to Page 2")
+# Title and description
+st.title("Tips dataset")
+st.markdown("This dashboard shows the tips dataset from the seaborn library.")
 
-def page3():
-    st.title("Page 3")
-    st.write("Welcome to Page 3")
+# Histogram of tips by day
+hist_values = data["tip"].groupby(data["day"]).sum()
+st.bar_chart(hist_values)
 
-# Create navigation sidebar
-st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Page 1", "Page 2", "Page 3"])
+# Scatter plot of tips vs. total bill
+scatter_data = alt.Chart(data).mark_circle().encode(
+    x='total_bill',
+    y='tip',
+    color='day',
+    tooltip=['total_bill', 'tip', 'day']
+).properties(
+    width=800,
+    height=400
+).interactive()
+st.altair_chart(scatter_data)
 
-# Display the appropriate page based on user selection
-if page == "Page 1":
-    page1()
-elif page == "Page 2":
-    page2()
-elif page == "Page 3":
-    page3()
+# Box plot of tips by time and sex
+box_data = data[['tip', 'time', 'sex']]
+box_plot = alt.Chart(box_data).mark_boxplot().encode(
+    x='time',
+    y='tip',
+    color='sex'
+).properties(
+    width=800,
+    height=400
+).interactive()
+st.altair_chart(box_plot)
