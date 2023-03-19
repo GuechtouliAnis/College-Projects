d6ee64174f0b10eb60047073400e6d2a4f4089b2
Anis Guechtouli <81176327+GuechtouliAnis@users.noreply.github.com>
2023-03-19 15:51:10 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,3 +1,26 @@
 import streamlit as st
 
-st.write('Hello world!')
+# Define function for each page
+def page1():
+    st.title("Page 1")
+    st.write("Welcome to Page 1")
+
+def page2():
+    st.title("Page 2")
+    st.write("Welcome to Page 2")
+
+def page3():
+    st.title("Page 3")
+    st.write("Welcome to Page 3")
+
+# Create navigation sidebar
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ["Page 1", "Page 2", "Page 3"])
+
+# Display the appropriate page based on user selection
+if page == "Page 1":
+    page1()
+elif page == "Page 2":
+    page2()
+elif page == "Page 3":
+    page3()
